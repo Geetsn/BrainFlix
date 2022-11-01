99c1553186be5c456dab54726a3401922afb0787
geet <geeti_hashemi@yahoo.com>
2022-11-01 16:44:58 -0700

    edit app.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-          Learn React
-        </a>
-      </header>
+      hello react
     </div>
   );
 }
